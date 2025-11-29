ff3bd16c35570c5b202647b0b7ea5a44bc97fbeb
nickyonge <nick@duckpond.studio>
2025-11-28 22:19:19 -0800

    webpack.config.js overhaul

--- webpack.config.js
+++ webpack.config.js
@@ -22,23 +22,23 @@
  * 
  * Once prereqs are fulfilled, and this file (webpack.config.js) is placed in a new directory: 
  * 
- * 1: Run the following terminal commands:         (use Ctrl+` / Cmd+` to open the terminal)
+ * 1: Run the following three terminal commands:         (use Ctrl + ` / Cmd + ` to open the terminal)
         npm init -y
-            (If you get error text saying "The term 'npm' is not recognized", ensure node.js is installed)
-        npm install webpack webpack-cli --save
-        npm install html-webpack-plugin --save
-        npm install webpack-dev-server --save
-        npm install style-loader css-loader postcss-loader --save
-        npm install jquery --save
+ *          (If you get error text saying "The term 'npm' is not recognized", ensure node.js is installed)
+        npm i webpack webpack-cli webpack-dev-server webpack-remove-empty-scripts css-loader html-webpack-plugin mini-css-extract-plugin postcss-loader --save-dev
+        npm i typescript jquery --save
  * 2: Add the following values to package.json's "scripts":{} array:
-        "watch": "webpack --watch",
         "start": "webpack serve --open",
-        "build": "webpack"
-            (package.json will be auto-created in your parent directory. You'll find the scripts array 
-            inside it with a few values prepopulated - just copy+paste these!)
- * 3: Uncomment the JQuery and CSS { sections } of the module: {rules: { ... }} below
-            (This step is optional, but recommended as JQuery and CSS are very popular webdev resources)
-            (There are also modules for loading fonts and images you can use too <3 )
+        "start:dev": "webpack serve --mode=development --open",
+        "start:prod": "webpack serve --mode=production --open",
+        "build": "webpack --mode=production",
+        "preview": "npx http-server dist"
+ *         (package.json will be auto-created in your parent directory. You'll find the scripts array 
+ *         inside it with a few values prepopulated - just copy+paste these!)
+ * 3: Fill out the values in the Config region of this file, below
+        PRODUCTION_BUILD should be while you're in development mode 
+        SITE_TITLE should be the name of your beautiful new website
+        SRC_FOLDER, OUTPUT_FOLDER, and INDEX_FILE should be left "src", "dist", and "./js/index.js", respectively
  * 
  *       Done! Run command "npm start" to get underway
  *       Consider starting by making a src/ directory and index.js inside of it (steps 9/10 in the detailed instructions below)
@@ -51,25 +51,32 @@
  * 1: Create new folder (project, vscode workspace, github repo, etc), and place this file in it
  * 2: install npm
         npm init -y
- * 3: initialize webpack
-        npm install webpack webpack-cli --save
- * 4: initialize html webpack plugin, to auto-generate .html files in ./dist folder upon build 
-        npm install html-webpack-plugin --save
- * 5: initialize webpack dev server, for local testing at url "localhost:8080" (while watching)
-        npm install webpack-dev-server --save
+ * 3: initialize webpack and its CLI as dev dependencies 
+        npm install webpack webpack-cli --save-dev
+ * 4: initialize webpack dev server as a dev dependencies, for local testing at url "localhost:8080"
+        npm install webpack-dev-server --save-dev
+ * 5: initialize HTML Webpack Plugin as a dev dependency, to auto-generate .html files in ./dist folder upon build 
+        npm install html-webpack-plugin --save-dev
  * 6: add the following values to package.json's "scripts": {} array
-        "build": "webpack",
         "start": "webpack serve --open",
-        "watch": "webpack --watch",
- *          to enable terminal commands "npm run build" and "npm run watch", respectively 
- * 7: optionally, prep CSS loader, to import .css files from ./src
-        npm install style-loader css-loader postcss-loader --save
- *          then uncomment the CSS { section } from module rules below
- * 8: optionally, import the JQuery library, 
-        npm install jquery --save
- *          then uncomment the JQuery { section } from module rules below
- * 9: create a ./src subfolder, and inside it, a file called index.js
- * 10: uncomment the line "index: './src/index.js'," under entry { } below
+        "start:dev": "webpack serve --mode=development --open",
+        "start:prod": "webpack serve --mode=production --open",
+        "build": "webpack --mode=production",
+        "preview": "npx http-server dist"
+ *          to enable terminal commands "npm run build" and "npm run start", respectively 
+ * 7: Optionally, prep CSS loader, PostCSS loaders and Mini CSS Extract Plugin, all as dev dependencies, to import/export .css files from ./src
+        npm install css-loader postcss-loader mini-css-extract-plugin --save-dev
+ *          If not doing this step, you can remove CSS / mini plugin references from this file 
+ *          Note: We're using mini-css-extract-plugin instead of style-loader to control CSS file output 
+ * 8: Optionally, import the JQuery and TypeScript libraries, 
+        npm install jquery typescript
+ *          Note: Omit the --save-dev! These aren't dev-only dependencies.
+ *          If not doing this step, you can remove the JQuery references from this file. 
+ * 9: create a ./src subfolder with a /js subfolder, and inside it, a file called index.js 
+ * 10: Fill out the values in the Config region of this file, below
+        PRODUCTION_BUILD should be while you're in development mode 
+        SITE_TITLE should be the name of your beautiful new website
+        SRC_FOLDER and INDEX_FILE should be left "src" and "./js/index.js", respectively
  * 
  * 
  * 
@@ -86,65 +93,83 @@
 
 // tutorial complete~
 // done? consider deleting the above tutorial section, or moving it to the end of this file.
-// you may end up modifying webpack config quite a bit! might as well have the content up top. 
+// you may end up modifying webpack config quite a bit! might as well have the content up top.
 
 // https://webpack.js.org/configuration/
 
-const path = require('path');
-const webpack = require('webpack');                                  // const convenience reference to the local filepath
-
-const HtmlWebpackPlugin = require('html-webpack-plugin');        // const reference to webpack's html plugin
-const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // 
-const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts'); // ← add
-
+// #region Config 
 
 /** Export webpack bundle in Production mode, or Development? 
- * 
- * **Note:** Remember to close & restart Webpack if changed, `npm start` @returns {boolean} */
+ * - **Note:** Remember to close & restart Webpack if changed, via `npm start` 
+ * @see {@linkcode environment} for runtime mode detection @type {boolean} */
 const PRODUCTION_BUILD = false;
 
+/** Your site title; the text that will appear in the browser tab. @type {string} */
+const SITE_TITLE = 'Duck Pond Field Guide';
+
+/** Name of your source code folder. It should be in the same directory as `webpack.config.js` 
+ * itself. Don't put `webpack.config.js` *in* the source folder. @type {string} */
+const SRC_FOLDER = 'src';
+
+/** Name of the folder where build output will be exported. @type {string} */
+const OUTPUT_FOLDER = 'dist';
+
+/** Filepath of your index script, relative to 
+ * {@linkcode SRC_FOLDER}. Must begin with `./` or `/`. @type {string} */
+const INDEX_FILE = './js/index.js';
+
+// #endregion Config 
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+
+/** 
+ * Defines the runtime build environment
+ * 
+ * To determine what the current environment is in your codebase, use:  
+ * `process.env.NODE_ENV`  
+ * which will return a string matching either `"production"` or 
+ * `"development"`, depending on the value of {@linkcode PRODUCTION_BUILD}.
+   @example
+   const _env = process.env.NODE_ENV;
+   const isDevBuild = _env === 'development';
+   if (isDevBuild) { 
+       // Do secret sneaky dev-only stuff here! 
+   }
+ */
+const environment = PRODUCTION_BUILD ? 'production' : 'development';
 
 module.exports = () => {
 
-    const mode = PRODUCTION_BUILD ? 'production' : 'development'; // argv.mode || 'development';
-    // PRODUCTION_BUILD
-
     return {
 
-        mode: mode,
-        // mode: PRODUCTION_BUILD ? 'production' : 'development',
+        // set the runtime build environment 
+        mode: environment,
 
-        context: path.resolve(__dirname, 'src'),
+        // reference to src folder build path 
+        context: path.resolve(__dirname, SRC_FOLDER),
 
-        // enable Watch Mode (auto refresh changes),
-        // watch: true, // see: https://webpack.js.org/guides/development/#using-watch-mode
-        // commenting out as watch performance is implied via devServer
-
-        entry: { //                                         entry: place to begin generating webpage from
-            index: './js/index.js',
+        entry: { // initial script to begin generating runtime webpage from
+            index: INDEX_FILE,
         },
         plugins: [
+            // define webpack plugins here 
             new RemoveEmptyScriptsPlugin(),
             new HtmlWebpackPlugin({
-                //                                          webpage title here
-                title: 'Duck Pond Field Guide',
+                title: SITE_TITLE,
             }),
             new MiniCssExtractPlugin({
                 filename: '[name].css',
                 chunkFilename: '[name].css',
                 runtime: false,
             }),
-
-            // define a plugin to ensure globally accessible __DEV__ and __PROD__ flags (prolly redundant, maybe remove later)
-            new webpack.DefinePlugin({
-                __DEV__: JSON.stringify(!PRODUCTION_BUILD),
-                __PROD__: JSON.stringify(PRODUCTION_BUILD),
-            }),
         ],
 
         // enable inline source mapping, so we can see lines/error info in browser console output
         devtool: PRODUCTION_BUILD ? 'source-map' : 'eval-cheap-source-map', // see: https://webpack.js.org/guides/development/#using-source-maps
-        // for prod builds, either use 'source-map' (full exposure of source in separate file, easy live debugging) or false (no sourcemap included)
+        // for prod builds, either use 'source-map' (full sourcemap in separate file, unsecure but easy live debugging) or false (no sourcemap included)
         // see: https://webpack.js.org/configuration/devtool/#devtool
 
         // enable webpack dev server so we can locally test 
@@ -152,15 +177,13 @@ module.exports = () => {
         // remember to also add check "optimization" at bottom
         // see: https://webpack.js.org/guides/development/#using-webpack-dev-server
         devServer: {
-            static: 'dist',
-            // static: path.resolve(__dirname, 'dist'),
+            static: OUTPUT_FOLDER,
         },
 
         module: {
             rules: [
-                //                                          module rules (with some presets)
 
-                // Jquery (requires library): npm install jquery --save
+                // Jquery 
                 {
                     test: require.resolve("jquery"),
                     loader: "expose-loader",
@@ -169,7 +192,7 @@ module.exports = () => {
                     },
                 },
 
-                // CSS (requires loader): npm install style-loader css-loader postcss-loader --save
+                // CSS loading 
                 {
                     test: /\.(sc|c)ss$/i,
                     use: [
@@ -195,16 +218,16 @@ module.exports = () => {
 
             ],
         },
+
         output: {
-            // filename: '[name].bundle.js',
             filename: '[name].bundle.js',
-            path: path.resolve(__dirname, 'dist'),
+            path: path.resolve(__dirname, OUTPUT_FOLDER),
             assetModuleFilename: '[path][name][ext]',
             clean: true,
         },
+
         // build time optimization, see https://webpack.js.org/guides/development/#using-webpack-dev-server 
         optimization: {
-            // runtimeChunk: 'single',
             runtimeChunk: false,
             splitChunks: {
                 cacheGroups: {
