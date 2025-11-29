6dbc64d49ccf48e467c4c908cb15bb0be53efa49
nickyonge <nick@duckpond.studio>
2025-11-28 19:11:20 -0800

    Initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Duck Pond Field Guide
+
+Digital content for the Duck Pond Field Guide art project
---
+++ src/index.js
@@ -0,0 +1,10 @@
+/* initial commit */
+
+/** 
+ * Demo function confirming import. 
+ * Outputs a console log. 
+ * @returns {void} 
+ */
+export function HelloWaddle() {
+    console.log('Hello, waddle! 🦆💖')
+}
