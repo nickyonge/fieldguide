21e844faec63ef512b0869f8a85dec8f7bd4d7a1
nickyonge <nick@duckpond.studio>
2025-11-30 15:05:57 -0800

    remove cost from multioption + toggle

--- src/js/components/multioption.js
+++ src/js/components/multioption.js
@@ -1,19 +1,13 @@
 import * as ui from "../ui";
 import { TitledComponent } from "./base";
-import * as cost from '../costs';
 
 export class MutliOptionList extends TitledComponent {
 
     #listSelect;
     #inputs;
     #labels;
-    #costs;
-    #costsP;
 
-    #costArray;
-    #currentCost;
-
-    constructor(componentTitle, onSelectCallback, options, costs, icons, initialValue = 0, horizontal = false) {
+    constructor(componentTitle, onSelectCallback, options, icons, initialValue = 0, horizontal = false) {
         super(componentTitle);
 
         ui.AddClassesToDOM(this.div, 'multichoicelist');
@@ -21,11 +15,6 @@ export class MutliOptionList extends TitledComponent {
 
         this.#inputs = [];
         this.#labels = [];
-        this.#costs = [];
-        this.#costsP = [];
-
-        this.#costArray = costs;
-        this.#currentCost = 0;
 
         if (!options) {
             console.warn("WARNING: creating a multi-option list without any options array");
@@ -55,15 +44,6 @@ export class MutliOptionList extends TitledComponent {
             text.innerHTML = options[i];
             label.appendChild(text);
 
-            // add costs tokens
-            let oCost = ui.CreateDivWithClass('cost');
-            let oCostP = ui.CreateElement('p');
-            oCost.appendChild(oCostP);
-            this.#costs.push(oCost);
-            this.#costsP.push(oCostP);
-            label.appendChild(oCost);
-            oCost.hidden = true;
-
             ui.DisableContentSelection(text);
             ui.MakeTabbableWithInputTo(label, input);
 
@@ -79,8 +59,6 @@ export class MutliOptionList extends TitledComponent {
         // check horizontal class
         if (horizontal) {
             ui.AddClassToDOMs('horizontal', this.div, this.#listSelect);
-            //TODO: check horizontal with cost tokens (currently untested)
-            //Issue URL: https://github.com/nickyonge/evto-web/issues/42
         }
 
         // add to document 
@@ -94,50 +72,6 @@ export class MutliOptionList extends TitledComponent {
 
         // add help component
         this.addHelpIcon(`help me! ${componentTitle}`);
-
-        // load initial costs 
-        this.UpdateCosts();
-    }
-
-    /**
-     * Load or reload cost values based on the current size 
-     */
-    UpdateCosts() {
-        if (this.#costArray == null || !Array.isArray(this.#costArray)) {
-            return;
-        }
-        let costArray = cost.GetCostArray(this.#costArray);
-        let len = this.#costsP.length;
-        if (costArray.length != this.#costsP.length) {
-            console.warn(`WARNING: array size mismatch between costsArray (${costArray.length}) and cost paragraphs (${this.#costsP.length}), can only update SOME tokens`);
-            len = Math.min(this.#costsP.length, costArray.length);
-        }
-
-        for (let i = 0; i < len; i++) {
-            if (costArray[i] == null) {
-                this.#costs[i].hidden = true;
-                this.#currentCost = 0;
-            } else {
-                this.#costs[i].hidden = false;
-                let _cost = costArray[i];
-                if (_cost < -99) { _cost = -99; } else if (_cost > 999) { _cost = 999; }
-                if (_cost < -9 || _cost > 99) {
-                    ui.AddClassToDOMs('tinyText', this.#costs[i]);
-                } else if (_cost < 0 || _cost > 9) {
-                    ui.AddClassToDOMs('smallText', this.#costs[i]);
-                } else {
-                    ui.RemoveClassesFromDOM(this.#costs[i], 'smallText', 'tinyText');
-                }
-                this.#costsP[i].innerText = _cost.toString();
-                this.#currentCost = _cost;
-            }
-        }
-    }
-
-    /** current token cost for this component 
-     * @returns {number} */
-    get cost() {
-        return this.#currentCost;
     }
 
     set selection(sel) {
--- src/js/components/toggle.js
+++ src/js/components/toggle.js
@@ -1,30 +1,20 @@
 import * as ui from "../ui";
 import { TitledComponent } from "./base";
-import * as cost from '../costs';
 
 export class Toggle extends TitledComponent {
 
     #input;
     #label;
-    #cost;
-    #costP;
     #switch;
-
-    #costArray;
-    #currentCost;
 
     /**
      * Create a new Toggle component, toggle switch that can be clicked on/off 
      * @param {string} [componentTitle = undefined] 
      * @param {(checked?:boolean, targetID?:string, target?:Element) => void} [onChangeCallback = undefined]
-     * @param {number[]|null} [cost = undefined] 
      * @param {boolean} [initialState = false] 
      */
-    constructor(componentTitle, onChangeCallback, cost, initialState = false) {
+    constructor(componentTitle, onChangeCallback, initialState = false) {
         super(componentTitle, false);
-
-        this.#costArray = cost;
-        this.#currentCost = 0;
 
         ui.AddClassesToDOM(this.div, 'toggle');
         this.#input = ui.CreateInputWithID('checkbox', String(this.uniqueComponentID), 'toggle');
@@ -36,13 +26,6 @@ export class Toggle extends TitledComponent {
         ui.MakeTabbableWithInputTo(this.#switch, this.#input);
 
         this.#input.checked = initialState;
-
-        // add costs field 
-        this.#cost = ui.CreateDivWithClass('cost', 'inline', 'darkBG');
-        this.#costP = ui.CreateElement('p');
-        this.#cost.appendChild(this.#costP);
-        this._titleElement.appendChild(this.#cost);
-        this.#cost.hidden = true;
 
         this.div.appendChild(this.#input);
         this.div.appendChild(this.#label);
@@ -57,39 +40,6 @@ export class Toggle extends TitledComponent {
             });
         }
         this.addHelpIcon(`help me! ${1}`, true, false);
-
-        this.UpdateCosts();
-    }
-
-    UpdateCosts() {
-        if (this.#costArray == null || !Array.isArray(this.#costArray)) {
-            return;
-        }
-        let costArray = cost.GetCostArray(this.#costArray);
-
-        if (costArray == null) {
-            this.#cost.hidden = true;
-            this.#currentCost = 0;
-        } else {
-            this.#cost.hidden = false;
-            let _cost = costArray[0];
-            if (_cost < -99) { _cost = -99; } else if (_cost > 999) { _cost = 999; }
-            if (_cost < -9 || _cost > 99) {
-                ui.AddClassToDOMs('tinyText', this.#cost);
-            } else if (_cost < 0 || _cost > 9) {
-                ui.AddClassToDOMs('smallText', this.#cost);
-            } else {
-                ui.RemoveClassesFromDOM(this.#cost, 'smallText', 'tinyText');
-            }
-            this.#costP.innerText = String(_cost);
-            this.#currentCost = _cost;
-        }
-    }
-
-    /** current token cost for this component 
-     * @returns {number} */
-    get cost() {
-        return this.#currentCost;
     }
 
     get checked() {
