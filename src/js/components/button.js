93da1e84fa9fd356ffcc5768f8c2fe5907e11527
nickyonge <nick@duckpond.studio>
2026-01-07 16:57:43 -0800

    button js p much done, onClickCallback

--- src/js/components/button.js
+++ src/js/components/button.js
@@ -1,4 +1,4 @@
-import { TitledComponent } from "./base";
+import { BasicComponent, TitledComponent } from "./base";
 import * as ui from '../ui';
 
 export class Button extends TitledComponent {
@@ -26,15 +26,37 @@ export class Button extends TitledComponent {
     /** Local definition for {@linkcode text} @type {string} */
     #text = '';
 
+    /** Gets this button's `<button>` HTML div @type {HTMLElement} */
+    get button() { return this.#button; }
+    /** `<button>` div @type {HTMLElement} */
     #button;
+    /** Drop shadow div @type {HTMLElement} */
     #shadow;
+    /** Edge of the button div @type {HTMLElement} */
     #edge;
+    /** Face of the button div, where text goes @type {HTMLElement} */
     #front;
+    /** Extended area for easier cursor selection @type {HTMLElement} */
     #hoverArea;
 
-    constructor(text, title) {
+    /** 
+     * Function called whenever the button is clicked 
+     * @type {(component?:Button) => void} 
+     */
+    onClickCallback;
 
-        super(title, false);
+    /**
+     * Interactive button component 
+     * @param {string} text Text to appear on the button's face 
+     * @param {(component?:Button) => void} [onClickCallback = undefined]
+     */
+    constructor(text, onClickCallback) {
+
+        super(text, false);
+
+        if (onClickCallback) {
+            this.onClickCallback = onClickCallback;
+        }
 
         this.#button = ui.CreateElementWithClass('button', 'pushable');
         this.#shadow = ui.CreateElementWithClass('span', 'shadow');
@@ -47,9 +69,23 @@ export class Button extends TitledComponent {
         this.#button.appendChild(this.#edge);
         this.#button.appendChild(this.#front);
         this.div.appendChild(this.#button);
-        
-        this.text = text;
 
+        this.#button.addEventListener('click', this.#onClick);
+        this.text = text;
+    }
+
+    /**
+     * on click event 
+     * @param {PointerEvent} event 
+     */
+    #onClick(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        if (this.onClickCallback) {
+            this.onClickCallback(this);
+        } else {
+            console.warn("Button was clicked, but no onClickCallback is defined", this);
+        }
     }
 
     #updateText() {
